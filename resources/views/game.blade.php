be7ddd99001110e50e14dd5ec3e383ebfb9a1c45
AbelRod01 <abel199601@gmail.com>
2023-04-11 18:07:02 -0400

    se hace un cambio en vista game

--- resources/views/game.blade.php
+++ resources/views/game.blade.php
@@ -1,8 +1,11 @@
 <x-layouts.app title="Game" meta-description="Game page">
 
 
-
-    Juego
+    <div class="row">
+        <div class="col-md-6 mx-auto text-center">
+            <h1 >Game</h1>
+        </div>
+    </div>
 
 
 
